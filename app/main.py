3732515d02971c7bedc8d9e80afe84be2bd0b360
VlBl <vladpuku@gmail.com>
2025-05-27 19:23:29 +0300

    Test changes

--- app/main.py
+++ app/main.py
@@ -5,11 +5,3 @@ def add(a, b):
 # Якщо файл запущено напряму, виконується приклад використання
 if __name__ == "__main__":
     print("Результат:", add(3, 5)) # Очікувано: 8
-
-
-def print_hi(name):
-    print(f'Hi, {name}')
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
