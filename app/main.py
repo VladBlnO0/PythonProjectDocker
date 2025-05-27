e1255f5e9322f1ad94887b5fc414d4c1e387ca6f
VlBl <vladpuku@gmail.com>
2025-05-27 19:11:02 +0300

    Main commit

---
+++ app/main.py
@@ -0,0 +1,15 @@
+def add(a, b):
+
+    return a + b
+
+# Якщо файл запущено напряму, виконується приклад використання
+if __name__ == "__main__":
+    print("Результат:", add(3, 5)) # Очікувано: 8
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
